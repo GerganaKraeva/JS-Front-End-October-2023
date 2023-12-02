9cbf9b37f51bd29eef6d9feeab0aa6ace0a8a672
GerganaKraeva <gergana.evg.kraeva@gmail.com>
2023-12-02 17:54:46 +0200

    Create Task6.js

---
+++ Exercise4/Task6.js
@@ -0,0 +1,41 @@
+function wordTracker (input) {
+
+    let stringList = input.shift().split(` `);
+    let occurances = {};
+
+    stringList.forEach((word) => {
+        occurances[word]=0;
+
+        input.forEach((element)=>{
+            if (word===element) {
+                occurances[word] +=1;
+            }
+        });
+        
+    });
+    
+    let entries = Object.entries(occurances).sort((a,b) => b[1] -a[1]);
+    
+    for (const [key, value] of entries) {
+        console.log(`${key} - ${value}`)
+        
+    }
+}
+
+
+
+
+
+wordTracker([
+
+    'this sentence',
+    
+    'In', 'this', 'sentence', 'you', 'have',
+    
+    'to', 'count', 'the', 'occurrences', 'of',
+    
+    'the', 'words', 'this', 'and', 'sentence',
+    
+    'because', 'this', 'is', 'your', 'task'
+    
+    ]);
