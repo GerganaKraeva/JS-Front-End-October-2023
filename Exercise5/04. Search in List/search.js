7ff7d230a4c34db21707734142f18b2a75ddd3c5
GerganaKraeva <gergana.evg.kraeva@gmail.com>
2023-12-06 00:20:48 +0200

    Update search.js

--- Exercise5/04. Search in List/search.js
+++ Exercise5/04. Search in List/search.js
@@ -3,13 +3,12 @@ function search() {
    const input = document.getElementById('searchText').value;
    const result = document.getElementById('result')
    let counter = 0;
+
+
    for (const town of towns) {
       town.style.textDecoration = '';
       town.style.fontWeight = '';
 
-   }
-
-   for (const town of towns) {
       if (town.textContent.includes(input)) {
          town.style.textDecoration = 'underline';
          town.style.fontWeight = 'bold';
