5bfa26069155ddf4c9440cb6ccc542b825d5da2b
GerganaKraeva <gergana.evg.kraeva@gmail.com>
2023-12-07 00:21:01 +0200

    solutions

---
+++ Exercise5/06. Format the Text/solution.js
@@ -0,0 +1,15 @@
+function solve() {
+  let input = document.getElementById('input').value.split('.');
+  let output = document.getElementById('output');
+
+
+  input = input.filter(s => s.length > 0)
+    .map(s => s += '.');
+
+  while (input.length > 0) {
+    let p = document.createElement('p');
+    p.textContent = input.splice(0, 3).join('');
+    output.appendChild(p);
+  }
+
+}
