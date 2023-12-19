798bf0bb487b8878a92f4f35db963757c43e3d3c
GerganaKraeva <gergana.evg.kraeva@gmail.com>
2023-12-19 16:46:33 +0200

    solution

---
+++ ExamPrep/motogp.js
@@ -0,0 +1,87 @@
+function solve(input) {
+    const riderCount = Number(input.shift());
+    const riders = {};
+    for (let index = 0; index < riderCount; index++) {
+        const riderLine = input.shift();
+   
+        const [name, fuel, position] = riderLine.split('|');
+
+        riders[name] = {
+            fuel: Number(fuel),
+            position: Number(position)
+        };
+    }
+
+    let commandLine = input.shift();
+
+    while (commandLine !== 'Finish') {
+        commandLine = commandLine.split(' - ');
+        const command =commandLine.shift();
+
+        switch ( command) {
+            case 'StopForFuel': {
+                const riderName = commandLine.shift();
+                const minimumFuel = Number(commandLine.shift());
+                const changePosition = Number(commandLine.shift());
+
+                if(riders[riderName].fuel < minimumFuel) {
+                    riders[riderName].position = changePosition;
+                    console.log(`${riderName} stopped to refuel but lost his position, now he is ${changePosition}.`)
+                } else {
+                    console.log(`${riderName} does not need to stop for fuel!`)
+                }
+                break;
+            }
+            case 'Overtaking': {
+                const firstRider = commandLine.shift();
+                const secondRider = commandLine.shift();
+               
+                const firstRiderPosition = riders[firstRider].position;
+                const secondRiderPosition = riders[secondRider].position;
+
+                if( firstRiderPosition < secondRiderPosition) {
+                    riders[firstRider].position=secondRiderPosition;
+                    riders[secondRider].position=firstRiderPosition;
+                    console.log(`${firstRider} overtook ${secondRider}!`);
+
+                }
+
+
+            break;
+            }
+            case 'EngineFail': {
+                const riderName = commandLine.shift();
+                const lapsLeft = commandLine.shift();
+
+                delete riders[riderName];
+                 
+                console.log(`${riderName} is out of the race because of a technical issue, ${lapsLeft} laps before the finish.`)
+                break;
+            }
+        }
+        commandLine = input.shift();
+    }
+
+    for (const riderName in riders) {
+        console.log(riderName);
+        console.log(`  Final position: ${riders[riderName].position}`);
+    }
+
+
+}
+
+solve(["3",
+
+    "Valentino Rossi|100|1",
+
+    "Marc Marquez|90|2",
+
+    "Jorge Lorenzo|80|3",
+
+    "StopForFuel - Valentino Rossi -50 - 1",
+
+    "Overtaking - Marc Marquez - Jorge Lorenzo",
+
+    "EngineFail - Marc Marquez - 10",
+
+    "Finish"]);
